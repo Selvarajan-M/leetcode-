aa2a7c307d98f4423e4b566cf797f4d530ecac66
SELVANAGARAJAN M <144997943+Selvarajan-M@users.noreply.github.com>
2025-02-13 22:11:03 +0530

    Time: 14 ms (52.86%), Space: 63.3 MB (22.45%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen=new Set();
+    for(let n of nums){
+        if(seen.has(n)) return true;
+        seen.add(n);
+    }
+    return false;
+};
